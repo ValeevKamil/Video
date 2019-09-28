1cc13b7a51038c8411f86ed578d8e1880fed4c1d
student@itpark.ru <Julia@localhost>
2019-09-28 10:42:27 +0300

    photo and video

--- app.js
+++ app.js
@@ -1,1 +1,29 @@
-console.log('worked')
+console.log('worked');
+
+// абстракция убираем все лишнее
+
+const author = {
+    id: 'buzova86',
+    avatarUrl: 'https://scontent-arn2-1.cdninstagram.com/vp/e24c6f401983834e27247c3542022660/5E1B7CB0/t51.2885-19/s150x150/69672885_368328154091538_8485268536803983360_n.jpg?_nc_ht=scontent-arn2-1.cdninstagram.com',
+    verified: true,
+};
+
+const photo = {
+    id: 'B23d82NooNF',
+    author: author,
+    location: 'Набережная Амура.Владивосток',
+    imageUrl: 'https://placekitten.com/800/800',
+    videoUrl: null,
+    likes: 159594,
+};
+
+
+const video = {
+    id: 'B23d82NooNfvF',
+    author: author,
+    location: 'Тц',
+    imageUrl: 'https://placekitten.com/800/800',
+    videoUrl: 'https://youtobe.com/...',
+    likes: 159594,
+};
+
