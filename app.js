0c8329196baf5af1ea60e4227b11d23c0d2591b2
student@itpark.ru <Julia@localhost>
2019-09-28 10:57:12 +0300

    переименовали переменную

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ console.log('worked');
 
 // абстракция убираем все лишнее
 
-const author = {
+const buzova = {
     id: 'buzova86',
     avatarUrl: 'https://scontent-arn2-1.cdninstagram.com/vp/e24c6f401983834e27247c3542022660/5E1B7CB0/t51.2885-19/s150x150/69672885_368328154091538_8485268536803983360_n.jpg?_nc_ht=scontent-arn2-1.cdninstagram.com',
     verified: true,
@@ -10,7 +10,7 @@ const author = {
 
 const photo = {
     id: 'B23d82NooNF',
-    author: author,
+    author: buzova,
     location: 'Набережная Амура.Владивосток',
     imageUrl: 'https://placekitten.com/800/800',
     videoUrl: null,
@@ -20,7 +20,7 @@ const photo = {
 
 const video = {
     id: 'B23d82NooNfvF',
-    author: author,
+    author: buzova,
     location: 'Тц',
     imageUrl: 'https://placekitten.com/800/800',
     videoUrl: 'https://youtobe.com/...',
